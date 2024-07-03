f74a6381bd2c6ab43038f800dc6df5e498176193
Bobbyarin1 <142946819+Bobbyarin1@users.noreply.github.com>
2024-07-03 07:10:05 -0700

    blackmamba

--- clientui/vstrm_server.py
+++ clientui/vstrm_server.py
@@ -1,5 +1,5 @@
 ####################################################################################
-# BLACKMAMBA BY: LOSEYS (https://github.com/loseys)
+# BLACKMgAMBA BY: LOSEYS (https://github.com/loseys)
 #
 # QT GUI INTERFACE BY: WANDERSON M.PIMENTA (https://github.com/Wanderson-Magalhaes)
 # ORIGINAL QT GUI: https://github.com/Wanderson-Magalhaes/Simple_PySide_Base
